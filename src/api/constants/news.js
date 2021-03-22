2317f03f672da43e0d0b4c5c17854c6a40e88c92
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-03-22 23:32:28 +0300

    pain

--- src/api/constants/news.js
+++ src/api/constants/news.js
@@ -4,10 +4,17 @@
 //   `http://newsapi.org/v2/everything?q=tesla&from=2021-03-08&sortBy=publishedAt&apiKey=${API_KEY}`;
 
 
-const API_KEY = 'c423e1cd-fe70-482f-ac65-6c8acdbfd38e';
+// const API_KEY = 'c423e1cd-fe70-482f-ac65-6c8acdbfd38e';
 
-export const REQUEST_URL =
-  `https://content.guardianapis.com/search?api-key=${API_KEY}`;
+// export const REQUEST_URL =
+//   `https://content.guardianapis.com/search?api-key=${API_KEY}`;
+
+
+  const API_KEY = '2118aa88-c2e0-4edb-8580-81184aad6054';
+
+  export const REQUEST_URL =
+    `https://content.guardianapis.com/search?api-key=${API_KEY}`;
+
 
 // const API_KEY = 'c56080bb5fabe39826c025db2ff4e6f5';
 
