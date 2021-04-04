26b2d759241135306e0fb77955b72b7a952dd32f
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-04-04 23:08:51 +0300

    fix

--- src/api/News/provider.js
+++ src/api/News/provider.js
@@ -6,5 +6,4 @@ export const getNews = () =>
   // fetch(REQUEST_URL)
   //   .then((response) => response.json())
   //   .then((data) => converter(data.response.results))
-  //   .catch(() => MOCK_NEWS);
   Promise.resolve(() => MOCK_NEWS)
